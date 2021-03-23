b1c188565691cc378bc9a2a3e2d33271803252b5
Syancheg <syancheg@gmail.com>
2021-03-23 16:02:40 +0300

    first work

---
+++ FirstWork/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  FirstWork
+//
+//  Created by Константин Кузнецов on 23.03.2021.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
